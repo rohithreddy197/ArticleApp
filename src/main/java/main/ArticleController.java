673ffdcee7c5842f4b482cb0f67c038c4328f0c9
rohithreddy <rohitreddy197@gmail.com>
2019-06-17 14:30:30 +0530

    Exception handling and base URLs added

--- src/main/java/main/ArticleController.java
+++ src/main/java/main/ArticleController.java
@@ -1,20 +1,66 @@
 package main;
 
 import model.Article;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpStatusCodeException;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
+import java.util.logging.Level;
+
+import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
 
 @RestController
 public class ArticleController {
+    Logger logger = LoggerFactory.getLogger(ArticleController.class);
+
     @Autowired
-    private  ArticleService articleService;
+    private ArticleService articleService;
 
     @RequestMapping("/articles")
-    public List<Article> getAllArticles()
-    {
+    public List<Article> getAllArticles() {
         return articleService.getAllArticles();
     }
+
+    @RequestMapping("/articles/{id}")
+    public Article getArticle(@PathVariable String id, HttpServletResponse response) {
+        try {
+            return articleService.getArticle(id);
+        } catch (Exception exc) {
+            logger.info("article with id not found");
+            return  null;
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/articles")
+    public void addArticle(@RequestBody Article article) {
+        try {
+            articleService.addArticle(article);
+        } catch (Exception exc) {
+            logger.info("Could not add new article");
+
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/articles/{id}")
+    public void updateArticle(@RequestBody Article article, @PathVariable String id) {
+        try {
+            articleService.updateArticle(article, id);
+        } catch (Exception exc) {
+            logger.info("article with id not found");
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/articles/{id}")
+    public void deleteArticle(@PathVariable String id) {
+        try {
+            articleService.deleteArticle(id);
+        } catch (Exception exc) {
+            logger.info("article with id not found to deleteit");
+        }
+    }
 }
+
